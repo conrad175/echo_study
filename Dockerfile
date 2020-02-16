3e94667d73be666c2847f0e31ae70aa8d319d6fe
Conrad <pppttab@gmail.com>
2020-02-16 22:55:06 +0900

    Add Redis

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang:1.12.0
 
 RUN go get github.com/labstack/echo/...
+RUN go get github.com/gomodule/redigo/redis
 
 WORKDIR /app
 ADD . /app
--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-  "net/http"
-  "github.com/labstack/echo"
+	"fmt"
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/gomodule/redigo/redis"
 )
 
+
+func Connection() redis.Conn {
+	const Addr = "redis:6379"
+	c, err := redis.Dial("tcp", Addr)
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
+
+func Set(key, value string, c redis.Conn) string{
+	res, err := redis.String(c.Do("SET", key, value))
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
 func main() {
-  e := echo.New()
-  e.GET("/sample", func(c echo.Context) error {
-    return c.String(http.StatusOK, "echo sample !\n")
-  })
-  e.Logger.Fatal(e.Start(":80"))
+	c := Connection()
+	defer c.Close()
+	res_set := Set("sample-key", "sample-value", c)
+	fmt.Println(res_set) 
+
+	e := echo.New()
+	e.GET("/sample", func(c echo.Context) error {
+	  return c.String(http.StatusOK, "echo sample !\n")
+	})
+	e.Logger.Fatal(e.Start(":80"))
 }
